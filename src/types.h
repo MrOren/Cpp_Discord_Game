1be6df85ab95610abd5640e21497937a48efd82f
Oren D <orendr02@gmail.com>
2017-09-23 12:31:41 +0300

    Update and rename Types.h to types.h

--- src/types.h
+++ src/types.h
@@ -1,15 +1,11 @@
 #include <string>
 #include <memory>
-
 using UInt = unsigned int;
 using String = std::string;
-
-
 template<typename T, typename Deleter = std::default_deleter<T>> using UniquePtr = std::unique_ptr<T, Deleter>;
 template<typename T, typename Deleter = std::default_deleter<T>, typename ... Args> UniquePtr<T, Deleter> constexpr CreateUniquePtr(Args&&... args){
   return UniquePtr<T, Deleter>(new T(args...));
 }
-
 template<typename T> using SharedPtr = std::shared_ptr<T>;
 template<typename T, typename ... Args> SharedPtr<T> constexpr CreateSharedPtr(Args&&... args){
   return std::make_shared<T>(args...);
