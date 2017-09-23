9bb38e6329ac86099b07cf473a37cd9eed932151
Oren D <orendr02@gmail.com>
2017-09-23 12:44:05 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,16 @@
 #include "player.h"
 
 template<typename T>
-T prompt(std::string message) {
-    std::cout << message;
-    T value;
-    std::cin >> value;
-    return value;
+T prompt(T&& message = String) {
+	std::cout << message;
+	T value;
+	std::cin >> value;
+	return value;
 }
-int main() {
-    std::string name;
-    name = prompt<std::string>("Enter player name: ");
-    std::cout << "Are you sure " << name << " is your name?\n";
+int main(int argc, char** argv) {
+	String name = prompt<std::string>("Enter player name: ");
+	std::cout << "Are you sure " << name << " is your name?\n";
 
-    return 0;
+	getchar();
+	return 0;
 }
