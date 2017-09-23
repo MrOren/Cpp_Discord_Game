6aeb1107ac2ae88e1557e77d89068cf72cdadc80
Oren D <orendr02@gmail.com>
2017-09-23 12:44:35 +0300

    Update player.h

--- src/player.h
+++ src/player.h
@@ -1,14 +1,15 @@
 #pragma once
 
 #include <string>
+#include "types.h"
 
-class player{
- public:
-  std::string getName();
-  void setName(const std::string& newName);
-  unsigned int getHealth();
-  void setHealth(const unsigned int newHealth);
- private:
-  std::string name;
-  unsigned int health;
+class player {
+public:
+	String getName() const;
+	void setName(const String& newName);
+	UInt getHealth() const;
+	void setHealth(const UInt newHealth);
+private:
+	String name;
+	UInt health;
 };
